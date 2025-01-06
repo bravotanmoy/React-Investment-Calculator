b9b21057977657330960949d65afe286570e2aa2
Tanmoy Biswas <tanmoyb.cse@gmail.com>
2025-01-06 09:54:48 +0600

    create result table

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,3 +1,32 @@
+import { calculateInvestmentResults, formatter } from "../util/investment"
+
 export default function Result({ input }) {
-    return <p>Result....</p>
+    const resultData = calculateInvestmentResults( input );
+    const initialInvestment = resultData[0].valueEndOfYear - resultData[0].interest - resultData[0].annualInvestment;
+    console.log(resultData);
+    
+    
+    return <table id="result">
+        <thead>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+        </thead>
+        <tbody>
+            {resultData.map(yearData => {
+                const totalInterest = yearData.valueEndOfYear - yearData.annualInvestment * yearData.year - initialInvestment;
+                
+                const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+                return <tr key={yearData.year}> 
+                    <td>{yearData.year}</td>
+                    <td>{formatter.format(yearData.valueEndOfYear) }</td>
+                    <td>{formatter.format(yearData.interest) }</td>
+                    <td>{formatter.format(totalInterest) }</td>
+                    <td>{formatter.format(totalAmountInvested) }</td> 
+                </tr>
+            } )}
+        </tbody>
+    </table>;
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -10,21 +10,21 @@ export default function UserInput( {onChange, userInput} ) {
         <div className="input-group">
             <p>
                 <level>Initial Investment</level>
-                <input type="number" value={userInput.initialInvestment} required onChange={(event) => onChange('initialInvestment', event.target.value)} />
+                <input type="text" value={userInput.initialInvestment} required onChange={(event) => onChange('initialInvestment', event.target.value)} />
             </p>
             <p>
                 <level>Annual Investment</level>
-                <input type="number" value={userInput.annualInvestment} required onChange={(event) => onChange('annualInvestment', event.target.value)} />
+                <input type="text" value={userInput.annualInvestment} required onChange={(event) => onChange('annualInvestment', event.target.value)} />
             </p>
         </div>
         <div className="input-group">
             <p>
                 <level>Expected Investment</level>
-                <input type="number" value={userInput.expectedReturn} required onChange={(event) => onChange('expectedReturn', event.target.value)} />
+                <input type="text" value={userInput.expectedReturn} required onChange={(event) => onChange('expectedReturn', event.target.value)} />
             </p>
             <p>
                 <level>Annual Investment</level>
-                <input type="number" value={userInput.duration} required onChange={(event) => onChange('duration', event.target.value)} />
+                <input type="text" value={userInput.duration} required onChange={(event) => onChange('duration', event.target.value)} />
             </p>
         </div>
     </section>
